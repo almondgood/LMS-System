b76e3f641eaf5ed0a2563385718aadc0fe45816a
AlmondGood <jwt2358@naver.com>
2022-10-05 14:51:51 +0900

    환경설정

---
+++ src/main/java/lmssystem/lmssystem/IndexController.java
@@ -0,0 +1,13 @@
+package lmssystem.lmssystem;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello world!";
+    }
+}
---
+++ src/test/java/lmssystem/lmssystem/LmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lmssystem.lmssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
